01bed0a64c96edb32d9ada8c5d8af3313a425690
Matthew Morgenegg <noxpert@gmail.com>
2026-01-13 19:15:14 -0700

    Create djangolanguage/urls.py with Django URL configuration

---
+++ djangolanguage/urls.py
@@ -0,0 +1,17 @@
+"""
+Django URL configuration for djangolanguage application.
+
+This module defines URL patterns for the djangolanguage app.
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'djangolanguage'
+
+urlpatterns = [
+    # Add your URL patterns here
+    # Example:
+    # path('', views.index, name='index'),
+    # path('<int:id>/', views.detail, name='detail'),
+]
